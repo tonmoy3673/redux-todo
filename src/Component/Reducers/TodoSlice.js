2d20c2aa36d271e92e967a8a7242eac5b15b5055
tonmoy <tonmoy3673@gmail.com>
2025-01-10 18:38:23 +0600

    :bulb: set reducer

--- src/Component/Reducers/TodoSlice.js
+++ src/Component/Reducers/TodoSlice.js
@@ -1,9 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-const initialState=[{
-    id:1, text:'Hello Redux'
-}]
+const initialState={
+   todos:[{id:1, text:'Hello Redux'}] 
+}
 
 export const TodoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const todo = {id: nanoid(), text: action.payload};
+            state.todos.push(todo)
+        }
+    }
 
-})
+});
+
+export const {addTodo} = TodoSlice.actions;
+export default TodoSlice.reducer;
+
