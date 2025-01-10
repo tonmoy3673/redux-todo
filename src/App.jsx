72becaef092c1652289d4a181d48105b455d1b76
tonmoy <tonmoy3673@gmail.com>
2025-01-10 18:26:41 +0600

    :bulb: create initialState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from "./Component/Pages/Home"
+import Home from "./Component/Home/Home"
 
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-     <Home/>
+    <Home/>
     </>
   )
 }
---
+++ src/Component/Reducers/TodoSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState=[{
+    id:1, text:'Hello Redux'
+}]
+
+export const TodoSlice = createSlice({
+
+})
