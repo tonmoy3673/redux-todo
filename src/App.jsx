d73094ab6ffa25f607d89a500ca61fdd7f8cdd6d
tonmoy <tonmoy3673@gmail.com>
2025-01-12 21:13:16 +0600

    :tada: create add user page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./Component/Home/Home"
 import AddTodo from "./Component/Pages/AddTodo"
+import AddUsers from "./Component/Pages/AddUsers"
 import Counter from "./Component/Pages/Counter"
 import TodoList from "./Component/Pages/TodoList"
 import Users from "./Component/Pages/Users"
@@ -12,6 +13,7 @@ function App() {
     <>
     <Home/>
     <Users/>
+    <AddUsers/>
     <AddTodo/>
     <TodoList/>
     <Counter/>
---
+++ src/Component/Pages/AddUsers.jsx
@@ -0,0 +1,26 @@
+const AddUsers =()=>{
+    return (
+        <div>
+            <div>
+                <h3 className="text-md font-semibold text-center text-gray-600 py-3">Add User Info</h3>
+                <form>
+                   <div className="flex items-center justify-center space-x-8">
+                   <div className="flex space-x-2 items-center justify-center">
+                        <label htmlFor="name" className="text-gray-500 font-semibold">Name : </label>
+                        <input id="name" type="text" className="bg-gray-400 rounded-md placeholder-white px-2 py-[2px]" placeholder="Enter Your Name"/>
+                    </div>
+                    <div className="flex space-x-2 items-center justify-center my-3">
+                        <label htmlFor="email" className="text-gray-500 font-semibold">Email : </label>
+                        <input id="email" type="email" className="bg-gray-400 rounded-md placeholder-white px-2 py-[2px]" placeholder="Enter Your Email"/>
+                    </div>
+                   </div>
+                    <div className="text-center py-3 mb-4">
+                        <button className="text-sm bg-slate-600 py-2 px-3 text-white rounded-lg">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+};
+
+export default AddUsers;
